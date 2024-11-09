b17b127a517a132f5df3711933fc98cf7b69d086
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-11-09 16:30:54 +0300

    Added the my-company directory

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        {/* We can also use Navlink*/}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
