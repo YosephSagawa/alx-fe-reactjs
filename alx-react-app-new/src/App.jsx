c061218b1789b18bcd8beb0e836ec38ac0412612
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-11-09 10:53:35 +0300

    Added the Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import MainContent from "./components/MainContent.jsx";
 import Footer from "./components/Footer.jsx";
 import UserProfile from "./components/UserProfile.jsx";
 import "./App.css";
+import Counter from "./components/Counter.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
     </>
   );
