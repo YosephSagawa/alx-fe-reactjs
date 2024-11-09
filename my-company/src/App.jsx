e4b04aa5b6cf200b53a038c2b219919073d9792f
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-11-09 19:49:55 +0300

    Changed the router functionality

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,20 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  BrowserRouter,
+  Routes,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navbar />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="services" element={<Services />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route index element={<Home />} />
+    //     <Route path="about" element={<About />} />
+    //     <Route path="services" element={<Services />} />
+    //     <Route path="contact" element={<Contact />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,50 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 function Navbar() {
   return (
-    <nav
-      style={{
-        backgroundColor: "black",
-        padding: "10px",
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-    >
-      <ul
+    <>
+      <nav
         style={{
-          listStyle: "none",
+          backgroundColor: "black",
+          padding: "10px",
           display: "flex",
-          gap: "20px",
-          justifyContent: "start",
+          justifyContent: "space-between",
         }}
       >
-        <li>
-          <Link to={"/"} style={{ color: "white", textDecoration: "none" }}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to={"about"} style={{ color: "white", textDecoration: "none" }}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            to={"services"}
-            style={{ color: "white", textDecoration: "none" }}
-          >
-            Services
-          </Link>
-        </li>
-        <li>
-          <Link
-            to={"contact"}
-            style={{ color: "white", textDecoration: "none" }}
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        <ul
+          style={{
+            listStyle: "none",
+            display: "flex",
+            gap: "20px",
+            justifyContent: "start",
+          }}
+        >
+          <li>
+            <Link to={"/"} style={{ color: "white", textDecoration: "none" }}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"about"}
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"services"}
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={"contact"}
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
