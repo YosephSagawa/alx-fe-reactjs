0181579c109a67fec63b14a8a5a7a233622d54a5
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-11-24 21:53:02 +0300

    Removed the react-todo file

--- recipe-sharing-app/src/components/FavoritesList.jsx
+++ recipe-sharing-app/src/components/FavoritesList.jsx
@@ -2,16 +2,15 @@ import useRecipeStore from "./recipeStore";
 
 const FavoritesList = () => {
   const favorites = useRecipeStore((state) =>
-    state.favorites
-      .map((id) => state.recipes.find((recipe) => recipe.id === id))
-      .filter(Boolean)
+    state.favorites.map((id) =>
+      state.recipes.find((recipe) => recipe.id === id)
+    )
   );
 
   const removeFavorite = useRecipeStore((state) => state.removeFavorite);
 
   return (
     <div>
-      <h2>My Favorites</h2>
       {favorites.length === 0 ? (
         <p>No favorite recipes yet!</p>
       ) : (
