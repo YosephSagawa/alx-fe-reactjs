0906e258bb633591ca83f845724d356c76c67ffd
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-12-01 18:41:20 +0300

    Added the interactivity and animation

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 max-w-xs">
+    <div className="user-profile bg-gray-100 p-4 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 max-w-xs hover:shadow-xl">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24"
+        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 md:text-base sm:text-sm">
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
+      <p className="text-gray-600 md:text-base sm:text-sm ">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
