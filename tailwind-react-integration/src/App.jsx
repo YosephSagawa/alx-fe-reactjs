418ad799acfc0d10ee1149cdd39a2fe80e22c0ee
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-12-01 13:18:31 +0300

    Added the User Profile styles

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import UserProfile from "./components/UserProfile.jsx";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl">Hello World</h1>
+      <UserProfile />
     </>
   );
 }
