ffecc29e4bb1836fa78a1b92425a89c6889e6b67
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-12-06 09:20:37 +0300

    Added the AddRecipeForm Component

---
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+const AddRecipeForm = () => {
+  const [data, setData] = useState({
+    title: "",
+    ingredients: "",
+    instructions: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prevState) => ({ ...prevState, [name]: value }));
+
+    if (name === "title" && value.length < 3) {
+      setError("Title must be at least 3 characters long");
+    } else {
+      setError("");
+    }
+
+    if (name === "ingredients" && value.length < 3) {
+      setError("Ingredients must be at least 3 characters long");
+    } else {
+      setError("");
+    }
+
+    if (name === "instructions" && value.length < 3) {
+      setError("Instructions must be at least 3 characters long");
+    } else {
+      setError("");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (error) {
+      alert("Please fill out all the fields appropriately.");
+      return;
+    }
+    console.log("Form submitted with data:", data);
+  };
+
+  return (
+    <div>
+      <h1>Add Recipe</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="name"
+          id="title"
+          name="title"
+          value={data.title}
+          onChange={handleChange}
+        />
+        <label htmlFor="imgredients">Ingredients:</label>
+        <input
+          type="text"
+          id="ingredients"
+          name="ingredients"
+          value={data.ingredients}
+          onChange={handleChange}
+        />
+        <label htmlFor="instructions">Instructions:</label>
+        <input
+          type="text"
+          id="instructions"
+          name="instructions"
+          value={data.instructions}
+          onChange={handleChange}
+        />
+        <button type="submit">Add Recipe</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddRecipeForm;
